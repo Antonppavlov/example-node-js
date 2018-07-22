974b730ca37b5a59048da7339e4f0fd81651e29c
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-23 01:58:37 +0300

    Работа с потоками в Node JS https://www.youtube.com/watch?v=Sqk-zR9KS2Q&list=PL0lO_mIqDDFX0qH9w5YQIDV6Wxy0oawet&index=9

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-var http = require('http');
 var fs = require('fs');
 
+var myReadShort = fs.createReadStream('new-one-dr/war_and_peace.txt','utf8');
+var myWriteShort = fs.createWriteStream('new-one-dr/war_and_peace_new.txt');
 
-var server = http.createServer(function (request,response) {
-
-    console.log(`URL по которому быз запрос: ${request.url}`);
-
-    response.writeHead(200,
-      {'Content-Type': 'text/plain; charset=utf-8'}
-    )
-
-    response.end(fs.readFileSync('new-one-dir/text.txt','utf8'));
+myReadShort.on('data', function (chunk) {
+  console.log(chunk);
+  myWriteShort.write(chunk);
 });
-
-
-server.listen(3000, '127.0.0.1');
-console.log('Занял адрес 127.0.0.1:3000');
