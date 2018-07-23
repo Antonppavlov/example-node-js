f38df6f0148159a37c46f1d7e6328df6cbcbe53e
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-24 02:57:41 +0300

    пример с отображением контента постепенно

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var fs = require('fs');
+const fs = require('fs');
+const http = require('http');
 
-var myReadShort = fs.createReadStream('new-one-dr/war_and_peace.txt','utf8');
-var myWriteShort = fs.createWriteStream('new-one-dr/war_and_peace_new.txt');
+var server = http.createServer(function(require,response) {
+  response.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'});
+  var myReadShort = fs.createReadStream('new-one-dir/war_and_peace.txt','utf8');
+  myReadShort.pipe(response)
+})
 
-myReadShort.on('data', function (chunk) {
-  console.log(chunk);
-  myWriteShort.write(chunk);
-});
+server.listen(3000,'127.0.0.1');
