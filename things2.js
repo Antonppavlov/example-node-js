eb2aeb893e61f7cda790a7dc064dedd62d9a8a9a
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-24 03:00:55 +0300

    не актуально

--- things2.js
+++
@@ -1,9 +0,0 @@
-module.exports.array_counter = function(array) {
-   return "В массиве находится "+ array.length + " элементов"
-}
-
-module.exports.multiply  = function (x,y) {
-  return `${x} умножить на ${y} равняеться ${x * y}`;
-}
-
-module.exports.some_value = 45;
