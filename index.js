3243549b7da54abda8d6a8bd2ec8eef2706e8ec2
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-25 00:32:19 +0300

    commit

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const fs = require('fs');
 const http = require('http');
 
 var server = http.createServer(function(require,response) {
-  response.writeHead(200,{'Content-Type':'aplication/json; charset=utf-8'});
-  var obj = {model:"Audi", age: 1988, max_speed: 255};
-
-  response.end(JSON.stringify(obj));
+  response.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'});
+  var myReadShort = fs.createReadStream('new-one-dir/war_and_peace.txt','utf8');
+  myReadShort.pipe(response)
 })
 
 server.listen(3000,'127.0.0.1');
-console.log("Сайт запущен по адрессу: 127.0.0.1:3000");
