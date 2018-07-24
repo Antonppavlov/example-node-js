e1fde4e0a0ad9f16ffdd794f80e53f6c1a74b2f4
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-25 01:09:00 +0300

    Маршрутизация Node JS
    https://www.youtube.com/watch?v=kjg2I-ed8Ok&t=0s&list=PL0lO_mIqDDFX0qH9w5YQIDV6Wxy0oawet&index=12

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
 const http = require('http');
 
+const url = "127.0.0.1";
+const port = 3000;
+
 var server = http.createServer(function(require,response) {
-  response.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'});
-  var myReadShort = fs.createReadStream('new-one-dir/war_and_peace.txt','utf8');
-  myReadShort.pipe(response)
+  response.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+
+  if(require.url === "/index" || require.url === "/"){
+    fs.createReadStream(__dirname + '/html/index.html')
+    .pipe(response);
+  }else if(require.url === "/about"){
+    fs.createReadStream(__dirname + '/html/about.html')
+    .pipe(response);
+  }
+  else {
+      fs.createReadStream(__dirname + '/html/404.html')
+      .pipe(response);
+  }
+
 })
 
-server.listen(3000,'127.0.0.1');
+server.listen(port,url);
+console.log(`Запущен сервер по адресу: ${url}:${port}`);
