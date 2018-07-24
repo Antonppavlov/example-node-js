7acd0a9c75651cf68a671064499b74bd0c4cffd3
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-25 02:21:42 +0300

    14 - Использование шаблонизатора
    https://www.youtube.com/watch?v=FUf8a47ZT9Q&index=14&list=PL0lO_mIqDDFX0qH9w5YQIDV6Wxy0oawet

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ var app = express();
 const port = 3000;
 
 app.listen(port);
+app.set('view engine', 'ejs');
 
 app.get("/", function (request,respons) {
-  respons.send('This is home');
+  respons.sendFile(__dirname + '/html/index.html');
 });
 
-app.get("/news", function (request,respons) {
-  respons.send('This is news');
+app.get("/about", function (request,respons) {
+  respons.sendFile(__dirname + '/html/about.html');
 });
 
-app.get("/news/:name/:id", function (request,respons) {
-  respons.send(`Name is:${request.params.name}, Id is: ${request.params.id}`);
+app.get("/news/:id", function (request,respons) {
+  let obj = {
+          name: 'Anton',
+          age: 29,
+          array: ['зачем','я','этим','занимаюсь']};
+
+
+  respons.render('news',{newsId : request.params.id, obj:obj});
+
+
 });
 
 console.log(`Запущен сервер локально, port: ${port}`);
