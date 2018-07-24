21b0a314cdd98880fff545ff1bfdb00b5b5eb64f
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-24 03:13:15 +0300

    пример постепенной прогрузки html

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const http = require('http');
 
 var server = http.createServer(function(require,response) {
-  response.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'});
-  var myReadShort = fs.createReadStream('new-one-dir/war_and_peace.txt','utf8');
+  response.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+  var myReadShort = fs.createReadStream('html/index.html','utf8');
   myReadShort.pipe(response)
 })
 
 server.listen(3000,'127.0.0.1');
+console.log("Сайт запущен по адрессу: 127.0.0.1:3000");
