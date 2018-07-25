f9e33820289e9b526db0b94d3965184320f82a62
Anton Pavlov <antonpavlov@Antons-MacBook-Pro.local>
2018-07-26 01:47:30 +0300

    Создание HTML-формы и получение данных
    https://www.youtube.com/watch?v=Qgsda_qT6pw&index=16&list=PL0lO_mIqDDFX0qH9w5YQIDV6Wxy0oawet

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
 const port = 3000;
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.listen(port);
 app.use('/public', express.static('public'));
@@ -15,13 +18,22 @@ app.get("/about", function (request,respons) {
   respons.render('about');
 });
 
+// POST /login gets urlencoded bodies
+app.post('/about', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  console.log('email: ' + req.body.email);
+    console.log('password: ' + req.body.password);
+    console.log('checkbox: ' + req.body.checkbox);
+    res.render('about')
+})
+
 app.get("/news/:id", function (request,respons) {
   let obj = {
           name: 'Anton',
           age: 29,
           array: ['зачем','я','этим','занимаюсь']};
 
-
+          console.log(request.query);
   respons.render('news',{newsId : request.params.id, obj:obj});
 
 
